71759cbf27ad739728fa8734639cbecf1a7c9bc3
Roman Scherer <roman.scherer@nugg.ad>
2012-09-03 11:01:17 +0200

    Release 0.0.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-dpkg "0.0.1-SNAPSHOT"
+(defproject lein-dpkg "0.0.1"
   :description "Leiningen plugin to build Debian packages."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
