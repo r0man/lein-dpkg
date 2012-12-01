2749dd29a0f05af963b302d67aca0d44fc446acb
Roman Scherer <roman.scherer@burningswell.com>
2012-12-01 19:44:40 +0100

    Added project url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-dpkg "0.0.2"
   :description "Leiningen plugin to build Debian packages."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/r0man/lein-dpkg"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
