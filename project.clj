7fbe58fcb237e3943617e5b8b0b8fa015d4fbbce
Roman Scherer <roman.scherer@burningswell.com>
2013-03-01 20:22:53 +0100

    Release 0.1.0.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-dpkg "0.0.2"
+(defproject lein-dpkg "0.1.0"
   :description "Leiningen plugin for the Debian package management system"
   :url "https://github.com/r0man/lein-dpkg"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.0"]]
   :eval-in-leiningen true)
