06f305ebf058e1fcfc77528b4152e45d9dcdc0bc
Roman Scherer <roman@burningswell.com>
2015-04-20 15:46:47 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/r0man/lein-dpkg"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.apache.commons/commons-io "1.3.2"]]
+  :deploy-repositories [["releases" :clojars]]
   :eval-in-leiningen true
   :dpkg {:incremental "BUILD_NUMBER"}
-  ;; :plugins [[lein-dpkg "0.1.2-SNAPSHOT"]]
-  )
+  :min-lein-version "2.0.0")
